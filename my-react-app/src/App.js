c75fedae9348b0fac6d156aefe3d6cfc0555265b
jpate101 <62268987+jpate101@users.noreply.github.com>
2024-02-01 16:18:50 +1000

    tailwind play

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,6 +1,7 @@
 
 import WeatherForecast from './WeatherForecast';
 import React, { useState, useEffect } from 'react';
+import "./App.css"
 
 
 
@@ -10,7 +11,15 @@ const App = () => {
       <h1>Your React App</h1>
       <WeatherForecast />
 
+      <div>
+        <section className='h-screen bg-green-400 flex items-center justify-center m-12 text-center text-3xl'>
+          <p>tailwind test</p>
+        </section>
+      </div>
+
     </div>
+    
+    
   );
 };
 
